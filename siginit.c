f92d901ba61accc77274ff65f4e04ecd0f61146a
Yuncheng Peter Yao(CS@NYU) <yy4108@nyu.edu>
2022-10-08 20:09:25 +0800

    siginit.c compiled and tested

--- siginit.c
+++ siginit.c
@@ -1,23 +1,23 @@
 #include <signal.h>
-#include <stdion.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include "utility.h"
 
 void siginit(struct sigaction * def, sigset_t * mask, void (*handler)(int)){
-    struct sigaction * new;
+    struct sigaction new;
 
-    sigemptyset(def->sa_mask);
-    sigemptyset(new->sa_mask);
+    sigemptyset(&(def->sa_mask));
+    sigemptyset(&(new.sa_mask));
     sigemptyset(mask);
 
     def->sa_flags = 0;
     new.sa_flags = 0;
 
-    sigaddset(new.sa_mask, SIGINT);
-    sigaddset(new.sa_mask, SIGQUIT);
-    sigaddset(def->sa_mask, SIGQUIT);
-    sigaddset(def->sa_mask, SIGINT);
+    sigaddset(&(new.sa_mask), SIGINT);
+    sigaddset(&(new.sa_mask), SIGQUIT);
+    sigaddset(&(def->sa_mask), SIGQUIT);
+    sigaddset(&(def->sa_mask), SIGINT);
 
     sigaddset(mask, SIGINT);
     sigaddset(mask, SIGQUIT);
@@ -25,6 +25,12 @@ void siginit(struct sigaction * def, sigset_t * mask, void (*handler)(int)){
     def->sa_handler = SIG_DFL;
     new.sa_handler = handler;
 
-    sigaction(SIGINT, new, NULL);
-    sigaction(SIGQUIT, new, NULL);
+    sigaction(SIGINT, &new, NULL);
+    sigaction(SIGQUIT, &new, NULL);
 }
+
+// int main(){
+//     struct sigaction dfl;
+//     sigset_t mask;
+//     siginit(&dfl, &mask, NULL);
+// }
